3696c87704d7452566375fd81eec8d197bb4e958
kiking <kiking0501@gmail.com>
2024-09-10 01:08:25 -0400

    improve UI

--- main.js
+++ main.js
@@ -9,17 +9,8 @@ $( document ).ready(function() {
     var init_pos = 2;
     load_book(init_pos);
 
-    $("#expand_toggle").on('click', function() {
-        $("#section_display").removeClass("col-lg-5");
-        $("#section_reading_notes").removeClass("col-lg-7");
-        adjust_column_display();
-    });
-    $("#parallel_toggle").on('click', function() {
-        $("#section_display").addClass("col-lg-5");
-        $("#section_reading_notes").addClass("col-lg-7");
-        adjust_column_display();
-    });
-    
+    $("#expand_toggle").on('click', function() {adjust_column_display(true);});
+    $("#parallel_toggle").on('click', function() {adjust_column_display(false);});
 })
 
 function change_nav_pill(obj) {
@@ -35,9 +26,17 @@ function change_nav_pill(obj) {
     }
 }
 
-function adjust_column_display() {
-    $("#expand_toggle").toggle();
-    $("#parallel_toggle").toggle();
+function adjust_column_display(expand) {
+    if (expand) {
+        $("#expand_toggle").hide(); $("#parallel_toggle").show();
+        $("#section_display").removeClass("col-lg-6");
+        $("#section_reading_notes").removeClass("col-lg-6");
+    }
+    else {
+        $("#expand_toggle").show(); $("#parallel_toggle").hide();
+        $("#section_display").addClass("col-lg-6");
+        $("#section_reading_notes").addClass("col-lg-6");
+    }
     window.dispatchEvent(new Event('resize'));
 }
 
@@ -97,14 +96,8 @@ function display_graph() {
         if (link.type == "read_sequence") { return 'lightgrey' }
       })
       .linkOpacity(.5)
-      .linkWidth((link)=> {
-        if (link.type == "related") { return 1 };
-        return 0;
-      })
-      .linkDirectionalParticles((link)=> {
-        if (link.type == "related") { return 10};
-        return 0;
-      })
+      .linkWidth((link)=> { if (link.type == "related") { return 1 }; return 0; })
+      .linkDirectionalParticles((link)=> { if (link.type == "related") { return 10}; return 0; })
       .linkDirectionalParticleWidth(1.5)
       .linkDirectionalParticleSpeed(0.003)
       .nodeThreeObject((node) => {
@@ -120,13 +113,28 @@ function display_graph() {
       .onNodeClick((node) => {
         if (node.type == "text") {
             load_book(node.book_key, node.bookmark_page);
-            $("#message_selected_page .title").html(node.title);
+            $("#message_selected_page .title").html(node.bookmark_name);
             $("#message_selected_page .page").html(node.bookmark_page);
             $("#message_selected_page").show();
+
+            // Aim at node from outside it
+              const distance = 300;
+              const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+              const newPos = node.x || node.y || node.z
+                ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+                : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+              Graph.cameraPosition(
+                newPos, // new position
+                node, // lookAt ({ x, y, z })
+                2000  // ms transition duration
+              );
             
         } else {
             load_book(node.book_key);
-        }
+            $("#parallel_toggle").click();
+        };
       })
 
     Graph.d3Force('charge').strength(-80);
